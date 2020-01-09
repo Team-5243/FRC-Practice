db8083937602a84311ff9dd2a26d687e2ca73a28
Team-5243 <programming@centrevillerobotics.net>
2020-01-09 17:46:47 -0500

    [ADD] DriveSubsystem code

---
+++ Learning2/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+public class Constants {
+    public static final int FRONTRIGHT = 1;
+    public static final int FRONTLEFT = 2;
+    public static final int BACKLEFT = 3;
+    public static final int BACKRIGHT = 4;
+
+    public static final int CONTROLLER = 0;
+
+}
