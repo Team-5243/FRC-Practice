ec3fac07fcb2a9244a0bbc0ad11ef7e9bef0c2b9
Team-5243 <programming@centrevillerobotics.net>
2020-01-14 17:56:58 -0500

    [UPDATE] WPI 2020 code

---
+++ LearningChasis/src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class DriveCommand extends CommandBase {
+  final private DriveSubsystem m_driveSubsystem;
+  final private XboxController m_xboxController;
+
+  /**
+   * Creates a new DriveCommand.
+   */
+  public DriveCommand(DriveSubsystem subsystem, XboxController ankle){
+    m_driveSubsystem = subsystem;
+    m_xboxController = ankle;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_driveSubsystem.setMotors(m_xboxController.getY(Hand.kRight), m_xboxController.getY(Hand.kLeft));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_driveSubsystem.setMotors(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- LearningChasis/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ LearningChasis/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,35 +10,35 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-/**
- * Add your docs here.
- */
-public class DriveSubsystem extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-
+public class DriveSubsystem extends SubsystemBase {
+  
   CANSparkMax frontLeft, frontRight, backLeft, backRight;
-
+  
+  /**
+   * Creates a new DriveSubsystem.
+   */
   public DriveSubsystem() {
     frontLeft = new CANSparkMax(Constants.FRONTLEFT, MotorType.kBrushless);
     frontRight = new CANSparkMax(Constants.FRONTRIGHT, MotorType.kBrushless);
     backLeft = new CANSparkMax(Constants.BACKLEFT, MotorType.kBrushless);
     backRight = new CANSparkMax(Constants.BACKRIGHT, MotorType.kBrushless);
 
-    backLeft.follow(frontLeft);
     backRight.follow(frontRight);
+    backLeft.follow(frontLeft);
   }
 
-  public void setMotors(double rightSpeed, double leftSpeed) {
-    frontLeft.set(rightSpeed);
-    frontRight.set(leftSpeed);
+  public void setMotors(double right, double left) {
+    frontLeft.set(left);
+    frontRight.set(right);
+    backLeft.set(left);
+    backRight.set(right);
   }
 
   @Override
-  public void initDefaultCommand() {
-    
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
